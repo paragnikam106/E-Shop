6619e3e14e7f10248bc62bc4da3d3a71a607cd7a
Parag Nikam <paragnikam106@gmail.com>
2025-03-06 16:23:47 +0530

    add

--- jewelryshop/wsgi.py
+++ jewelryshop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E-shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelryshop.settings')
 
 application = get_wsgi_application()
