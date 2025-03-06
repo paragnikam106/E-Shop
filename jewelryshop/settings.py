688c597c1ba3f8ca55eaf6170b1c174edee73370
Parag Nikam <paragnikam106@gmail.com>
2025-03-06 09:03:32 +0530

    deploy

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['TitanParag.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
